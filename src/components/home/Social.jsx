05552a6db26c5529b55b4d771fab59dad96e8569
Rondinelle Cunha <rondinelle.cunha.101@ufrn.edu.br>
2023-08-09 19:02:34 -0300

    Adicionando seção Home

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Social() {
+  return (
+    <div>
+      <div className="home__social">
+        <a href="https://www.linkedin.com/in/rondinelle-cunha/" className="home__social-icon" target="_blank">
+            <i class="uil uil-linkedin"></i>
+        </a>
+
+        <a href="https://www.linkedin.com/in/rondinelle-cunha/" className="home__social-icon" target="_blank">
+            <i class="uil uil-github-alt"></i>
+        </a>
+
+      </div>
+    </div>
+  );
+}
+
+export default Social;
