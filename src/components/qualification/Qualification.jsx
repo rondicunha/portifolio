a1e8fb9d0f26ae908f01425b344a5a764f04df2d
Rondinelle Cunha <rondinelle.cunha.101@ufrn.edu.br>
2023-10-06 16:12:21 -0300

    Adicionando experiências profissionais

--- src/components/qualification/Qualification.jsx
+++ src/components/qualification/Qualification.jsx
@@ -67,20 +67,54 @@ function Qualification() {
 
                 <div className={toggleState === 2 ? "qualification__content qualification__content-active" : "qualification__content"}>
                     <div className="qualification__data">
-                        <div>
-                            <h3 className="qualification__title">Analista Requisitos (Estágio)</h3>
-                            <span className="qualification__subtitle"> STI - Superintendência de Tecnologia da Informação</span>
-                            <div className="qualification__calendar">
-                                <i className="uil uil-calendar-alt"></i>
-                                04/2021 - Atual
+                            <div>
+                                <h3 className="qualification__title">Desenvolvedor de Software </h3>
+                                <span className="qualification__subtitle"> IMD - Metrópole Digital UFRN</span>
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt"></i>
+                                    10/2023 - Atual
+                                </div>
+                            </div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
                             </div>
                         </div>
 
-                        <div>
-                            <span className="qualification__rounder"></span>
-                            <span className="qualification__line"></span>
+                        <div className="qualification__data">
+                            <div></div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+
+                            <div>
+                                <h3 className="qualification__title">Desenvolvedor Java</h3>
+                                <span className="qualification__subtitle"> ESIG Software</span>
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt"></i>
+                                    09/2023 - 10/2023
+                                </div>
+                            </div>
                         </div>
-                    </div>
+
+                        <div className="qualification__data">
+                            <div>
+                                <h3 className="qualification__title">Analista de Requisitos/Negócios </h3>
+                                <span className="qualification__subtitle"> STI - Superintendência de Tecnologia da Informação da UFRN </span>
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt"></i>
+                                    04/2021 - 09/2023
+                                </div>
+                            </div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+                        </div>
 
                     
                 </div>
